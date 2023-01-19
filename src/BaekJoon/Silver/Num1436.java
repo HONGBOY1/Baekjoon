f40196faab6d894596a69e21a3e9b6767082adf0
hongboy1 <hongboy15@gmail.com>
2023-01-19 14:29:43 +0900

    백준(2023/01-09) 실버5 1436(영화감독 숌/브루트포스)

---
+++ src/BaekJoon/Silver/Num1436.java
@@ -0,0 +1,21 @@
+package BaekJoon.Silver;
+
+import java.util.Scanner;
+
+public class Num1436 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while(count != n) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
