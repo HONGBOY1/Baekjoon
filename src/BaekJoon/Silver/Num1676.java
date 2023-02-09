d1c26482f1af940fe168d29761fd6bafcbe21559
hongboy1 <hongboy15@gmail.com>
2023-02-09 10:51:53 +0900

    백준(2023/01-09) 실버5 1676(팩토리얼 0개수)

---
+++ src/BaekJoon/Silver/Num1676.java
@@ -0,0 +1,18 @@
+package BaekJoon.Silver;
+
+
+import java.util.Scanner;
+
+public class Num1676 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        int n= sc.nextInt();
+        int cnt= 0;
+        while(n >= 5){
+            cnt += n /5;
+            n/=5;
+        }
+        System.out.println(cnt);
+    }
+}
