25bf2dd9aec7c07f8f4eee38db3b865e2f24b5f6
hongboy1 <hongboy15@gmail.com>
2023-03-07 12:02:28 +0900

    백준(2023/03-07) 브론즈4 6916(123456789)

---
+++ src/BaekJoon/Bronze/Num6916.java
@@ -0,0 +1,130 @@
+package BaekJoon.Bronze;
+
+import java.util.Scanner;
+
+public class Num6916 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num= sc.nextInt();
+
+        switch (num){
+            case 0 :
+                System.out.print(
+                        " * * *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *");
+                break;
+            case 1 :
+                System.out.print(
+                        "\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *");
+                break;
+            case 2 :
+                System.out.print(
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        " * * *\n" +
+                        "*\n" +
+                        "*\n" +
+                        "*\n" +
+                        " * * *");
+                break;
+            case 3 :
+                System.out.print(
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        " * * *");
+                break;
+            case 4 :
+                System.out.print(
+                        "\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *" );
+                break;
+            case 5 :
+                System.out.print(
+                        " * * *\n" +
+                        "*\n" +
+                        "*\n" +
+                        "*\n" +
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        " * * *");
+                break;
+            case 6 :
+                System.out.print(
+                        " * * *\n" +
+                        "*\n" +
+                        "*\n" +
+                        "*\n" +
+                        " * * *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *");
+                break;
+            case 7 :
+                System.out.print(
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *");
+                break;
+            case 8 :
+                System.out.print(
+                        " * * *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *");
+                break;
+            case 9 :
+                System.out.print(
+                        " * * *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        "*     *\n" +
+                        " * * *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        "      *\n" +
+                        " * * *");
+                break;
+        }
+    }
+}
